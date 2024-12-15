d334df3d744b927886bfed38b8023ec0774fdeff
Rohitsingh13131313 <153498704+Rohitsingh13131313@users.noreply.github.com>
2024-12-15 23:52:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let womenbg = document.getElementById("women");
+
+womenbg.addEventListener("click",function(){
+   womenbg.style.backgroundColor ="#525050"
+    
+})
+let menbg = document.getElementById("men");
+
+menbg.addEventListener("click",function(){
+   nbg.style.backgroundColor ="#525050"
+    
+})
